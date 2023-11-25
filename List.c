c9468663b6dc9651c61d02798a8b29bfcd9c5a6f
ymd <129515331+jxufeDD@users.noreply.github.com>
2023-11-25 23:33:49 +0800

    链表和顺序表的一些基本操作

--- List.c
+++ List.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAX_SIZE 100
 
 //实现线性表的基本操作
@@ -11,5 +12,91 @@ typedef struct {
 }SeqList;
 
 void initSeqList(SeqList *L) {
+	L->data[0] = 1;
+	L->data[1] = 2;
+	L->data[2] = 3;
+	L->data[3] = 4;
+	L->data[4] = 5;
+	L->length = 5;
+}
+
+
+void GetSeqList(SeqList L, int i, int *e) {
+	if (i<1 || i>L.length)
+		return;
+	*e = L.data[i - 1];
+}
+
+
+int LocateSeqList(SeqList L, int elem) {
+	int i;
+	for (i = 0; i < L.length; i++) {
+		if (L.data[i] == elem) {
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
+
+void insertSeqList(SeqList* L, int i, int elem) {
+	if (i<1 || i>L->length) return;
+	if (L->length == MAX_SIZE) return;
+	for (int j = L->length - 1; j >= i - 1; j--) {
+		L->data[j + 1] = L->data[j];
+	}
+	L->data[i - 1] = elem;
+	L->length++;
+}
+
+
+void insertSeqList2(SeqList L, int i, int elem) {
+	if (i<1 || i>L.length) return;
+	if (L.length == MAX_SIZE) return;
+	for (int j = L.length - 1; j >= i - 1; j--) {
+		L.data[j + 1] = L.data[j];
+	}
+	L.data[i - 1] = elem;
+	L.length++;
+}
+
+void showSeqList(SeqList L) {
+	printf("顺序表元素为：");
+	for (int i = 0; i < L.length; i++) {
+		printf("%d ", L.data[i]);
+	}
+	printf("\n");
+}
+
+
+void deleteSeqListElem(SeqList* L, int i) {
+
+}
+
+void testList1() {
+	SeqList mylist;
+	initSeqList(&mylist);
+	printf("初始态");
+	showSeqList(mylist);
+
+
+	printf("用值传递：");
+	insertSeqList2(mylist, 3, 666);
+	showSeqList(mylist);
+
+
+	printf("用地址传递：");
+	insertSeqList(&mylist, 3, 666);
+	showSeqList(mylist);
+
+	//获取第一个元素
+	int e;
+	GetSeqList(mylist, 1, &e);
+	printf("获取到的第一个元素为:%d\n", e);
+
+
+	//查找元素
+	int location=LocateSeqList(mylist, 666);
+	printf("666的位置在:%d", location);
 
 }
